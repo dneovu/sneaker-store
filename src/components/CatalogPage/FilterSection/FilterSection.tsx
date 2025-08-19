9ab2cffc107ea2ceae02a704b61c2ad65e2142bf
dneovu <113314302+dneovu@users.noreply.github.com>
2025-08-19 15:13:26 +0300

    feat: add skeleton loading for catalog filter

--- src/components/CatalogPage/FilterSection/FilterSection.tsx
+++ src/components/CatalogPage/FilterSection/FilterSection.tsx
@@ -11,6 +11,7 @@ import RangeInput from './RangeInput';
 import { fetchPriceRange } from '@/store/priceRangeSlice';
 import BrandsFilter from './BrandsFilter';
 import SizeFilter from './SizeFilter';
+import Skeleton from 'react-loading-skeleton';
 
 const FilterSection = () => {
   const dispatch = useAppDispatch();
@@ -47,21 +48,40 @@ const FilterSection = () => {
     }
   }, [dispatch, brandItems, sizeItems, priceRange]);
 
-  if (loadingBrands || loadingSizes || loadingPrices) return;
-
   return (
     <section className="sticky top-8">
       <div className="border-b border-gray-300 pb-6">
         <h2 className="mb-4 font-bold md:text-lg">Бренды</h2>
-        <BrandsFilter filterBrands={filterBrands} />
+        {loadingBrands ? (
+          <Skeleton count={4} height={20} className="mb-2" />
+        ) : (
+          <BrandsFilter filterBrands={filterBrands} />
+        )}
       </div>
+
       <div className="border-b border-gray-300 py-6">
         <h2 className="mb-4 font-bold md:text-lg">Стоимость</h2>
-        <RangeInput priceRange={priceRange} />
+        {loadingPrices ? (
+          <Skeleton height={12} />
+        ) : (
+          <RangeInput priceRange={priceRange} />
+        )}
       </div>
+
       <div className="pt-6">
         <h2 className="mb-4 font-bold md:text-lg">Размеры</h2>
-        <SizeFilter filterSizes={filterSizes} />
+        {loadingSizes ? (
+          <Skeleton
+            count={5}
+            width={45}
+            height={45}
+            inline
+            borderRadius={10}
+            className="mr-2 mb-2"
+          />
+        ) : (
+          <SizeFilter filterSizes={filterSizes} />
+        )}
       </div>
     </section>
   );
